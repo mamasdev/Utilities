22b74a3d781dfca541fb78f1f874c21bc5f7350d
Abderrahman Mamas <abderrahmanmamas@gmail.com>
2019-09-16 21:56:26 -0400

    Optimize OTL and GV and GT

--- ObjectToLeanear.js
+++ ObjectToLeanear.js
@@ -1,18 +1,77 @@
-function ObjectToLeanear(obj){
-    let sql=""
-    let keys=Object.keys(obj)
-    keys.forEach(function(e){
+
+function GetValues(obj) {
+    let sql = ""
+    let keys = Object.keys(obj)
+    keys.forEach(function (e) {
 
         let what;
-        if(typeof(obj[e])=="String")
-            what='"'+obj[e]+'" , '
-        else if(typeof(obj[e])=="Number")
-            what=obj[e]+" , "
-        else if(typeof(obj[e])=="Date")
-            what='`'+obj[e]+'` , '
+
+        if (typeof (obj[e]) == "string")
+            what = '"' + obj[e].replace(/\"/g, '\\"') + '" , '
+        else if (typeof (obj[e]) == "number")
+            what = obj[e] + " , "
+        else if (typeof (obj[e]) == "date")
+            what = '`' + obj[e] + '` , '
+        else {
+            if (obj[e] == null) {
+                what = '"" , '
+            } else {
+
+                console.log(e + " " + obj[e])
+            }
+        }
 
 
-        sql+= "["+ e +"] = "+what
+        sql += what
     })
-    return obj
+
+    sql = sql.substring(0, sql.length - 2)
+
+    return sql
+
 }
+
+function GetTitles(obj) {
+    let sql = ""
+    let keys = Object.keys(obj)
+    keys.forEach(function (e) {
+        sql += "`" + e + "`,"
+    })
+
+    sql = sql.substring(0, sql.length - 1)
+
+    return sql
+
+}
+
+function ObjectToLeanear(obj) {
+    let sql = ""
+    let keys = Object.keys(obj)
+    keys.forEach(function (e) {
+
+        let what;
+
+        if (typeof (obj[e]) == "string")
+            what = '"' + obj[e].replace(/\"/g, '\\"') + '" , '
+        else if (typeof (obj[e]) == "number")
+            what = obj[e] + " , "
+        else if (typeof (obj[e]) == "date")
+            what = '`' + obj[e] + '` , '
+        else {
+            if (obj[e] == null) {
+                what = '"" , '
+            } else {
+
+                console.log(e + " " + obj[e])
+            }
+        }
+
+
+        sql += "`" + e + "` = " + what
+    })
+
+    sql = sql.substring(0, sql.length - 2)
+
+    return sql
+}
+
